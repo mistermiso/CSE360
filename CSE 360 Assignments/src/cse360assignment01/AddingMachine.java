ff5134793cd95667d0fb60c7489ac3f5900c1ef9
Mitsu Fukuzaki <mfukuzak@asu.edu>
2020-10-04 19:22:56 -0700

    Initial check-in of AddingMachine.
    Change-Id: I518c666f3f4b887bf6435febb3f0f71f9151ec51

---
+++ CSE 360 Assignments/src/cse360assignment01/AddingMachine.java
@@ -0,0 +1,34 @@
+/* Author: Mitsu Fukuzaki
+ * Class: CSE360 (Tuesdays at 9:00am - 10:15am)
+ * Date: 10/04/2020
+ * Description: The purpose of this assignment is to familiarize ourself with using Github.
+ */
+
+
+package cse360assignment01;
+
+public class AddingMachine {
+	
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
